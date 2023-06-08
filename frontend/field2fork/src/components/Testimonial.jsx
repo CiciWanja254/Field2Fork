1b08bbcf980fd3cbc5c9667abf01f546311a845b
patrickwide <patrickwide254@gmail.com>
2023-06-09 02:23:06 +0300

    Retreave prev code

--- frontend/field2fork/src/components/Testimonial.jsx
+++ frontend/field2fork/src/components/Testimonial.jsx
@@ -1,89 +1,87 @@
+import React from "react";
+
+const posts = [
+  {
+    id: 1,
+    description:
+      "Field2Fork has revolutionized the way I connect with wholesalers and customers. It's a game-changer for my farm business, enabling me to reach a wider market and establish direct relationships. Highly recommended!",
+    author: {
+      name: "John Doe",
+      role: "Farmer",
+      imageUrl:
+        "https://i.pinimg.com/564x/7e/5b/bc/7e5bbcd6495bb6beac328abfd0242602.jpg",
+    },
+  },
+  {
+    id: 1,
+    description:
+      "As a wholesaler, Field2Fork has streamlined my sourcing process, connecting me directly with reliable farmers. The platform's efficiency and transparency have transformed the way I do business. It's a must-have for anyone in the wholesale industry.",
+    author: {
+      name: "Jane Smith",
+      role: "Wholesaler",
+      imageUrl:
+        "https://i.pinimg.com/564x/d4/15/1f/d4151f9385db87207ab017870983d8ef.jpg",
+    },
+  },
+  {
+    id: 1,
+    description:
+      "Field2Fork has made it incredibly convenient for me to access fresh, locally sourced produce. I love knowing exactly where my food comes from and supporting farmers in my community. The app is user-friendly, and the quality of the products is outstanding. I'm a loyal customer!",
+    author: {
+      name: "Mark Johnson",
+      role: "Consumer",
+      imageUrl:
+        "https://i.pinimg.com/564x/fa/65/2c/fa652c503dbd7260f2b457f6f7e3da65.jpg",
+    },
+  },
+  // More posts...
+];
+
 export default function Testimonial() {
   return (
-      <div>
-        <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-          <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-          <div className="mx-auto max-w-2xl lg:max-w-4xl">
-            <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" />
-            <figure className="mt-10">
-              <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-                <p>
-                "Field2Fork has revolutionized the way I connect with wholesalers and customers. It's a game-changer for my farm business, enabling me to reach a wider market and establish direct relationships. Highly recommended!"
-                </p>
-              </blockquote>
-              <figcaption className="mt-10">
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            User Testimonials
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Hear from our satisfied users who have experienced the benefits of
+            Field2Fork.
+          </p>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article
+              key={post.id}
+              className="flex max-w-xl flex-col items-start justify-between"
+            >
+              <div className="relative mt-4 flex items-center gap-x-4">
                 <img
-                  className="mx-auto h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  src={post.author.imageUrl}
                   alt=""
+                  className="h-10 w-10 rounded-full bg-gray-50"
                 />
-                <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                  <div className="font-semibold text-gray-900">John Doe</div>
-                  <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                    <circle cx={1} cy={1} r={1} />
-                  </svg>
-                  <div className="text-gray-600">Farmer</div>
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
                 </div>
-              </figcaption>
-            </figure>
-          </div>
-        </section>      <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-          <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-          <div className="mx-auto max-w-2xl lg:max-w-4xl">
-            <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" />
-            <figure className="mt-10">
-              <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-                <p>
-                "As a wholesaler, Field2Fork has streamlined my sourcing process, connecting me directly with reliable farmers. The platform's efficiency and transparency have transformed the way I do business. It's a must-have for anyone in the wholesale industry."
+              </div>
+
+              <div className="group relative">
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                  {post.description}
                 </p>
-              </blockquote>
-              <figcaption className="mt-10">
-                <img
-                  className="mx-auto h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                  <div className="font-semibold text-gray-900">Jane Smith</div>
-                  <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                    <circle cx={1} cy={1} r={1} />
-                  </svg>
-                  <div className="text-gray-600">Wholeseller</div>
-                </div>
-              </figcaption>
-            </figure>
-          </div>
-        </section>      <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-          <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-          <div className="mx-auto max-w-2xl lg:max-w-4xl">
-            <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" />
-            <figure className="mt-10">
-              <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-                <p>
-                "Field2Fork has made it incredibly convenient for me to access fresh, locally sourced produce. I love knowing exactly where my food comes from and supporting farmers in my community. The app is user-friendly, and the quality of the products is outstanding. I'm a loyal customer!"
-                </p>
-              </blockquote>
-              <figcaption className="mt-10">
-                <img
-                  className="mx-auto h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                  <div className="font-semibold text-gray-900">Mark Johnson</div>
-                  <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                    <circle cx={1} cy={1} r={1} />
-                  </svg>
-                  <div className="text-gray-600">Customer</div>
-                </div>
-              </figcaption>
-            </figure>
-          </div>
-        </section>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
--- frontend/field2fork/src/pages/ProductPage.jsx
+++ frontend/field2fork/src/pages/ProductPage.jsx
@@ -1,228 +1,32 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProductCard from "../components/ProductCard";
+import ProductLIst from "../components/ProductList";
+import SearchBar from "../components/SearchBar";
 
-import { Dialog, Transition } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
+export default function ProductPage({ products }) {
+  const parsedProducts = products.map((product) => JSON.parse(product));
+  const [searchQuery, setSearchQuery] = useState("");
 
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-export default function ProductPage() {
-  
-    const [open, setOpen] = useState(true)
-  
-    const openCart = () => {
-      setOpen(true);
-    };
-  
-    let products = [1,2,3,4,5,6,7,8];
-    return (
-      <div>
-        <Header />
-            <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
-                <button onClick={openCart}>Open Cart</button>
-                <br/>
-    
-                <form>
-                    <div class="flex">
-                        <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-                        <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">All categories <svg aria-hidden="true" class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
-                        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
-                                </li>
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
-                                </li>
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
-                                </li>
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="relative w-full">
-                            <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
-                            <button type="submit" class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                                <span class="sr-only">Search</span>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+  // Filter the products based on the search query
+  const filteredProducts = parsedProducts.filter((product) =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-            <div className="bg-white">
-                <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-
-                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                        {products.map((product, index) => (
-                            <ProductCard key={index} />                        
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-            {open && <Cart />}
-        <Footer />
+  return (
+    <div>
+      <Header />
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
+          <ProductLIst products={parsedProducts} handleSearch={handleSearch} />
+        </div>
       </div>
-    );
-
-
-    function Cart() {
-
-        const products = [
-            {
-            id: 1,
-            name: 'Throwback Hip Bag',
-            href: '#',
-            color: 'Salmon',
-            price: '$90.00',
-            quantity: 1,
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-            imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-            },
-            {
-            id: 2,
-            name: 'Medium Stuff Satchel',
-            href: '#',
-            color: 'Blue',
-            price: '$32.00',
-            quantity: 1,
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-            imageAlt:
-                'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-            },
-            // More products...
-        ]
-
-        return (
-            <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" onClose={setOpen}>
-                <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-500"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-500"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                >
-                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                </Transition.Child>
-
-                <div className="fixed inset-0 overflow-hidden">
-                <div className="absolute inset-0 overflow-hidden">
-                    <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="transform transition ease-in-out duration-500 sm:duration-700"
-                        enterFrom="translate-x-full"
-                        enterTo="translate-x-0"
-                        leave="transform transition ease-in-out duration-500 sm:duration-700"
-                        leaveFrom="translate-x-0"
-                        leaveTo="translate-x-full"
-                    >
-                        <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                        <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                            <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                            <div className="flex items-start justify-between">
-                                <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
-                                <div className="ml-3 flex h-7 items-center">
-                                <button
-                                    type="button"
-                                    className="-m-2 p-2 text-gray-400 hover:text-gray-500"
-                                    onClick={() => setOpen(false)}
-                                >
-                                    <span className="sr-only">Close panel</span>
-                                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                                </button>
-                                </div>
-                            </div>
-
-                            <div className="mt-8">
-                                <div className="flow-root">
-                                <ul role="list" className="-my-6 divide-y divide-gray-200">
-                                    {products.map((product) => (
-                                    <li key={product.id} className="flex py-6">
-                                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                        <img
-                                            src={product.imageSrc}
-                                            alt={product.imageAlt}
-                                            className="h-full w-full object-cover object-center"
-                                        />
-                                        </div>
-
-                                        <div className="ml-4 flex flex-1 flex-col">
-                                        <div>
-                                            <div className="flex justify-between text-base font-medium text-gray-900">
-                                            <h3>
-                                                <a href={product.href}>{product.name}</a>
-                                            </h3>
-                                            <p className="ml-4">{product.price}</p>
-                                            </div>
-                                            <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                                        </div>
-                                        <div className="flex flex-1 items-end justify-between text-sm">
-                                            <p className="text-gray-500">Qty {product.quantity}</p>
-
-                                            <div className="flex">
-                                            <button
-                                                type="button"
-                                                className="font-medium text-indigo-600 hover:text-indigo-500"
-                                            >
-                                                Remove
-                                            </button>
-                                            </div>
-                                        </div>
-                                        </div>
-                                    </li>
-                                    ))}
-                                </ul>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                            <div className="flex justify-between text-base font-medium text-gray-900">
-                                <p>Subtotal</p>
-                                <p>$262.00</p>
-                            </div>
-                            <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
-                            <div className="mt-6">
-                                <a
-                                href="#"
-                                className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                                >
-                                Checkout
-                                </a>
-                            </div>
-                            <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                                <p>
-                                or
-                                <button
-                                    type="button"
-                                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                                    onClick={() => setOpen(false)}
-                                >
-                                    Continue Shopping
-                                    <span aria-hidden="true"> &rarr;</span>
-                                </button>
-                                </p>
-                            </div>
-                            </div>
-                        </div>
-                        </Dialog.Panel>
-                    </Transition.Child>
-                    </div>
-                </div>
-                </div>
-            </Dialog>
-            </Transition.Root>
-        )
-    }
+      <Footer />
+    </div>
+  );
 }
-  
